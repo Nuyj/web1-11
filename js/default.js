9c1ccc11a6f490c49d0637e8ee19e48911bd128c
Nuyj <New.10_04@icloud.com>
2017-06-19 17:29:11 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -6,10 +6,10 @@ if(answer === number) {
   message = 'あたり！';
 }else if (answer < number){
   message = '残念！もっと大きいです';
-}else if (number > answer){
+}else if (number < answer){
   message = '残念！もっと小さいです';
 }else{
   message = '0~3の数字を入力してください';
 }
 
-document.getElementById('choice').tectContent = message;
+document.getElementById('choice').textContent = message;
