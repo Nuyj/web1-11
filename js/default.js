312dd0a04e744d26a1303de361843abd443992c9
Nuyj <New.10_04@icloud.com>
2017-06-19 17:21:34 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -2,5 +2,5 @@ var result = window.confirm('準備ok?');
 if(result){
 document.getElementById('choice').textContent = 'Okが押されました';
 }else{
-  document.getElementById('choice').textcontent = 'キャンセルが押されました';
+  document.getElementById('choice').textContent = 'キャンセルが押されました';
 }
